4565109e78472a3fb606a8bc501e63c602c82ed1
sakshi <sakshi.singh@qsstechnosoft.com>
2022-03-02 12:13:47 +0530

    Second commit

--- src/PokemonThumbnail.js
+++ src/PokemonThumbnail.js
@@ -1,42 +1,22 @@
 import React from 'react';
-import { CardGroup, Button,Card} from "react-bootstrap";
+import { CardGroup, Button, Card } from "react-bootstrap";
 import "./CardGrid.css"
- const PokemonThumbnail=({id,name,image,type})=> {
+const PokemonThumbnail = ({ id, name, image, type }) => {
+    return (<CardGroup>
 
-   console.log("props",id,name,type);
-  // console.log("xfvghnjm,jhngfdsadfghjhgf===",id,name,image,type);
-//         return (
-//          <div className={style}>
-//            <div className='number'>
-//            <small>#{id}</small>
-//            </div>
+        <Card className={"card-grid"} >
+            <div className='imgBlock'>
+                <Card.Img className={"card-img"} variant="bottom" src={image} />
+            </div>
+            <Card.Body className={'cardText'}>
+                <Card.Title className={"card-body"}><strong >{name}</strong></Card.Title>
+                <Card.Text>
+                    <p>{type}</p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
 
-// <img src={image} alt={name}/>
-// <div className='detail-wrapper'>
-//   <h1>
-//     {name}
-//   </h1>
-//   <small>Type:{type}</small>
-// </div>
-//          </div>
-        
-//         );
-  
-return(<CardGroup>
-
-          <Card className={"card-grid"} >
-              <div className='imgBlock'>
-              <Card.Img className={"card-img"} variant="bottom" src={image}/>
-              </div>
-              <Card.Body>
-                  <Card.Title><strong>{name}</strong></Card.Title>
-                  <Card.Text>
-                      <p>{type}</p>
-                  </Card.Text>
-              </Card.Body>
-          </Card>
-
-  </CardGroup>)
+    </CardGroup>)
 
 }
 export default PokemonThumbnail
